6ab8dc5bc0ffbbe3521e17566b7e7e035a446333
Collin Argo <collinargo@gmail.com>
2019-05-08 11:50:15 -0400

    more components, context

--- src/AddTarget/AddTarget.js
+++ src/AddTarget/AddTarget.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default () => {
     return (
-        <button>add target</button>
+        <button
+            onClick={() => null}
+        >add target</button>
     )
 }
--- src/TargetList/TargetList.js
+++ src/TargetList/TargetList.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import TrackerContext from '../Context/TrackerContext'
+import TargetListHeader from '../TargetListHeader/TargetListHeader';
+import Target from '../Target/Target'
 
 export default () => {
     return (
-        <h1>target list</h1>
+        <TrackerContext.Consumer>
+            {({ targets }) => 
+                <div>
+                    <h1>target list</h1>
+                    <TargetListHeader/>
+                    {targets.map(target => (
+                        <Target target={target}/>
+                    ))}
+                </div>
+            }
+        </TrackerContext.Consumer>
     )
 }
