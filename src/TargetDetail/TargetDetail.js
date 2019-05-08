2c89252afecb37373473db31a9486fa1b207a314
Collin Argo <collinargo@gmail.com>
2019-05-08 16:33:03 -0400

    touch outside to cancel target edit

--- src/TargetDetail/TargetDetail.js
+++ src/TargetDetail/TargetDetail.js
@@ -14,128 +14,134 @@ export default () => {
                 saveTarget, 
                 deleteTarget 
             }) => (
-                <form
-                    className='TargetDetail'
-                    onSubmit={event => {
-                        event.preventDefault()
-                        saveTarget()
-                    }}
-                    onReset={() => selectTarget(targets.find(target => target.id === selectedTarget.id))}
+                <div 
+                    className='targetDetailWrapper'
+                    onClick={() => selectTarget({})}
                 >
-                    <input 
-                        type='text' 
-                        name='name'
-                        value={selectedTarget.name}
-                        onChange={({ target }) => {
-                            editTarget({
-                                ...selectedTarget,
-                                name: target.value
-                            })
+                    <form
+                        className='TargetDetail'
+                        onClick={event => event.stopPropagation()}
+                        onSubmit={event => {
+                            event.preventDefault()
+                            saveTarget()
                         }}
-                    />
-                    <fieldset>
-                        <legend>Primary Contact</legend>
-                        <input 
-                            type='text' 
-                            name='contactName'
-                            value={selectedTarget.contacts[0].name}
-                            onChange={({ target }) => {
-                                editTarget({
-                                    ...selectedTarget,
-                                    contacts: [{
-                                        ...selectedTarget.contacts[0],
-                                        name: target.value
-                                    }]
-                                })
-                            }}
-                        />
-                        <input 
-                            type='text' 
-                            name='contactPhone'
-                            value={selectedTarget.contacts[0].phoneNumber}
-                            onChange={({ target }) => {
-                                editTarget({
-                                    ...selectedTarget,
-                                    contacts: [{
-                                        ...selectedTarget.contacts[0],
-                                        phoneNumber: target.value
-                                    }]
-                                })
-                            }}
-                        />
-                    </fieldset>
-                    <input 
-                        type='number'
-                        name='keyMetrics'
-                        value={selectedTarget.keyMetrics.revenue}
-                        onChange={({ target }) => {
-                            editTarget({
-                                ...selectedTarget,
-                                keyMetrics: {
-                                    ...selectedTarget.keyMetrics,
-                                    revenue: target.value
-                                }
-                            })
-                        }}
-                    />
-                    <fieldset 
-                        onChange={({ target }) => {
-                            editTarget({
-                                ...selectedTarget,
-                                status: target.value
-                            })
-                        }}
+                        onReset={() => selectTarget(targets.find(target => target.id === selectedTarget.id))}
                     >
-                        <legend>Target Status</legend>
-                        <label>
-                            Researching
-                            <input 
-                                type='radio'
-                                name='status'
-                                value='researching'
-                                checked={selectedTarget.status === statusTypes[0]}
-                            />
-                        </label>
-                        <label>
-                            Pending approval
-                            <input 
-                                type='radio'
-                                name='status'
-                                value='pending approval'
-                                checked={selectedTarget.status === statusTypes[1]}
-                            />
-                        </label>
-                        <label>
-                            Approved
-                            <input 
-                                type='radio'
-                                name='status'
-                                value='approved'
-                                checked={selectedTarget.status === statusTypes[2]}
-                            />
-                        </label>
-                        <label>
-                            Denied
-                            <input 
-                                type='radio'
-                                name='status'
-                                value='denied'
-                                checked={selectedTarget.status === statusTypes[3]}
-                            />
-                        </label>
-
-                    </fieldset>
-                    <div>
-                        <input type='submit' value='save'/>
-                        <input type='reset' value='reset'/>
-                        <input type='button' value='cancel'
-                            onClick={() => selectTarget({})}
-                            />
-                        <input type='button' value='delete'
-                            onClick={() => deleteTarget()}
+                        <input 
+                            type='text' 
+                            name='name'
+                            value={selectedTarget.name}
+                            onChange={({ target }) => {
+                                editTarget({
+                                    ...selectedTarget,
+                                    name: target.value
+                                })
+                            }}
                         />
-                    </div>
-                </form>
+                        <fieldset>
+                            <legend>Primary Contact</legend>
+                            <input 
+                                type='text' 
+                                name='contactName'
+                                value={selectedTarget.contacts[0].name}
+                                onChange={({ target }) => {
+                                    editTarget({
+                                        ...selectedTarget,
+                                        contacts: [{
+                                            ...selectedTarget.contacts[0],
+                                            name: target.value
+                                        }]
+                                    })
+                                }}
+                            />
+                            <input 
+                                type='text' 
+                                name='contactPhone'
+                                value={selectedTarget.contacts[0].phoneNumber}
+                                onChange={({ target }) => {
+                                    editTarget({
+                                        ...selectedTarget,
+                                        contacts: [{
+                                            ...selectedTarget.contacts[0],
+                                            phoneNumber: target.value
+                                        }]
+                                    })
+                                }}
+                            />
+                        </fieldset>
+                        <input 
+                            type='number'
+                            name='keyMetrics'
+                            value={selectedTarget.keyMetrics.revenue}
+                            onChange={({ target }) => {
+                                editTarget({
+                                    ...selectedTarget,
+                                    keyMetrics: {
+                                        ...selectedTarget.keyMetrics,
+                                        revenue: target.value
+                                    }
+                                })
+                            }}
+                        />
+                        <fieldset 
+                            onChange={({ target }) => {
+                                editTarget({
+                                    ...selectedTarget,
+                                    status: target.value
+                                })
+                            }}
+                        >
+                            <legend>Target Status</legend>
+                            <label>
+                                Researching
+                                <input 
+                                    type='radio'
+                                    name='status'
+                                    value='researching'
+                                    checked={selectedTarget.status === statusTypes[0]}
+                                />
+                            </label>
+                            <label>
+                                Pending approval
+                                <input 
+                                    type='radio'
+                                    name='status'
+                                    value='pending approval'
+                                    checked={selectedTarget.status === statusTypes[1]}
+                                />
+                            </label>
+                            <label>
+                                Approved
+                                <input 
+                                    type='radio'
+                                    name='status'
+                                    value='approved'
+                                    checked={selectedTarget.status === statusTypes[2]}
+                                />
+                            </label>
+                            <label>
+                                Denied
+                                <input 
+                                    type='radio'
+                                    name='status'
+                                    value='denied'
+                                    checked={selectedTarget.status === statusTypes[3]}
+                                />
+                            </label>
+
+                        </fieldset>
+                        <div>
+                            <input type='submit' value='save'/>
+                            <input type='reset' value='reset'/>
+                            <input type='button' value='cancel'
+                                onClick={() => selectTarget({})}
+                                />
+                            <input type='button' value='delete'
+                                onClick={() => deleteTarget()}
+                            />
+                        </div>
+                    </form>
+                </div>
             )}
         </TrackerContext.Consumer>
     )
