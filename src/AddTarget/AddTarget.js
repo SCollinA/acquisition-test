b4da9422d79c976b9db505aab921e2bf3505da66
Collin Argo <collinargo@gmail.com>
2019-05-08 11:59:13 -0400

    add new target

--- src/AddTarget/AddTarget.js
+++ src/AddTarget/AddTarget.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import TrackerContext from '../Context/TrackerContext'
 
 export default () => {
     return (
-        <button
-            onClick={() => null}
-        >add target</button>
+        <TrackerContext.Consumer>
+            {({ addTarget }) => (
+                <button
+                    onClick={addTarget}
+                >add target</button>
+            )}
+        </TrackerContext.Consumer>
     )
 }
--- src/Tracker/Tracker.js
+++ src/Tracker/Tracker.js
@@ -7,9 +7,17 @@ import TargetList from '../TargetList/TargetList'
 export default class extends React.Component {
     constructor(props) {
         super(props)
+        const addTarget = () => this.setState({
+            targets: [
+                ...this.state.targets,
+                {
+                    name: 'new target',
+                }
+            ]
+        })
         this.state = {
             targets: [],
-            addTarget: () => null,
+            addTarget,
             selectTarget: () => null,
             editTarget: () => null,
             deleteTarget: () => null,
