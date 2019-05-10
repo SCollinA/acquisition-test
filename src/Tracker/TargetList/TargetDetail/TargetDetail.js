894149a82fabb29204c47c0255c4fd7b74a32d25
Collin Argo <collinargo@gmail.com>
2019-05-10 11:47:33 -0400

    extract key metrics detail to component

--- src/Tracker/TargetList/TargetDetail/TargetDetail.js
+++ src/Tracker/TargetList/TargetDetail/TargetDetail.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './TargetDetail.css'
 import TrackerContext from '../../../Context/TrackerContext'
 import TargetEditButton from './TargetEditButton';
-import Contact from '../../Contact/Contact';
+import Contact from '../Contact/Contact';
+import KeyMetricsDetail from './KeyMetricsDetail';
 
 export default ({ target }) => {
     return (
@@ -16,13 +17,8 @@ export default ({ target }) => {
                     <h1>{target.name}</h1>
                     {target.contacts.map((contact, index) => (
                         <Contact key={index} contact={contact}/>
-                        // make a contact component
-                        // add 'add contact' button here
-                        // add 'edit contact' button here
-                        // add 'remove contact' button here
-                        // or have a contact modal
                     ))}
-                    <h1>{target.keyMetrics.revenue}</h1>
+                    <KeyMetricsDetail target={target}/>
                     <h1>{target.status}</h1>
                     {target.history.map((historyEntry, index) => (
                         <h2 className='historyEntry' key={index}>
