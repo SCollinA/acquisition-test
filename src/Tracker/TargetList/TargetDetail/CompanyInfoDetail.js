e7f493ba1881f47f5c3cd49027ad539742c3910a
Collin Argo <collinargo@gmail.com>
2019-05-14 08:31:52 -0400

    add minus sign for negative metrics values

--- src/Tracker/TargetList/TargetDetail/CompanyInfoDetail.js
+++ src/Tracker/TargetList/TargetDetail/CompanyInfoDetail.js
@@ -6,11 +6,11 @@ export default ({ target }) => {
             <h4>{target.info.name || 'No Name'}</h4>
             <h4>{target.info.address || 'No Address'}</h4>
             <h4>
-                {`${target.info.employeesCount} `}
+                {`${target.info.employeesCount.toLocaleString()} `}
                 Employee{target.info.employeesCount > 1 && 's'}
             </h4>
             <h4>{target.info.foundedDate && 
-                `Founded ${target.info.foundedDate}`}</h4>
+                `Founded ${new Date(target.info.foundedDate).toLocaleDateString()}`}</h4>
             <h4>{target.info.isPublic && 'Publicly Traded'}</h4>
         </div>
     )
