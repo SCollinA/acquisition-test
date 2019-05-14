82a99cc2cfb7c0fba4f49418c6d04026458eb60b
Collin Argo <collinargo@gmail.com>
2019-05-14 08:49:30 -0400

    show company info only if available

--- src/Tracker/TargetList/TargetDetail/CompanyInfoDetail.js
+++ src/Tracker/TargetList/TargetDetail/CompanyInfoDetail.js
@@ -5,13 +5,19 @@ export default ({ target }) => {
         <div className='CompanyInfoDetail'>
             <h4>{target.info.name || 'No Name'}</h4>
             <h4>{target.info.address || 'No Address'}</h4>
-            <h4>
-                {`${target.info.employeesCount.toLocaleString()} `}
-                Employee{target.info.employeesCount > 1 && 's'}
-            </h4>
-            <h4>{target.info.foundedDate && 
-                `Founded ${new Date(target.info.foundedDate).toLocaleDateString()}`}</h4>
-            <h4>{target.info.isPublic && 'Publicly Traded'}</h4>
+            {target.info.employeesCount &&
+                <h4>
+                    {`${target.info.employeesCount.toLocaleString()} `}
+                    Employee{target.info.employeesCount > 1 && 's'}
+                </h4>}
+            {target.info.foundedDate && 
+                <h4>
+                    Founded {new Date(target.info.foundedDate).toLocaleDateString()}
+                </h4>}
+            {target.info.isPublic && 
+                <h4>
+                    'Publicly Traded'
+                </h4>}
         </div>
     )
 }
